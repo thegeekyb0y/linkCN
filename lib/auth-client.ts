1a4d85e173cf8698efacd34606035eab122309ea
Aditya Tiwari <thegeekyboy.in@gmail.com>
2026-01-22 02:24:42 +0530

    .

---
+++ lib/auth-client.ts
@@ -0,0 +1,7 @@
+import { twoFactorClient } from "better-auth/plugins";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: "http://localhost:3000",
+  plugins: [twoFactorClient()],
+});
